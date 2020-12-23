7070ff27232b99eb5ac16510cb5ff3d0574cce4f
stronggeniusa@gmail.com <stronggeniusa@gmail.com>
2020-12-23 21:46:22 +0700

    Добавлен партатип альбомов в видео

---
+++ build/classes/file.js
@@ -0,0 +1,229 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _fsExtra = _interopRequireDefault(require("fs-extra"));
+
+var _crypto = _interopRequireDefault(require("crypto"));
+
+var _db = require("./db");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var _default = /*#__PURE__*/function () {
+  function _default() {
+    _classCallCheck(this, _default);
+  }
+
+  _createClass(_default, null, [{
+    key: "SaveFile",
+    //Сохраняем новый вайл в таблицу файлов и сам файл
+    value: function () {
+      var _SaveFile = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(fields, savePath) {
+        var hash, type, url, arFields, result;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _context.prev = 0;
+
+                if (!fields.old_file) {
+                  _context.next = 4;
+                  break;
+                }
+
+                _context.next = 4;
+                return this.Delete(fields.old_file, true);
+
+              case 4:
+                //хеш размера и имени файла
+                hash = _crypto["default"].createHash('md5').update(fields.file.size + fields.file.name).digest("hex"); //вытаскиваем расширение
+
+                type = fields.file.type.split('/');
+                type = type[1];
+                url = "files/".concat(fields.module_id, "/").concat(hash, ".").concat(type); //к основному пути прибавляем путь к модулю
+
+                savePath = "".concat(savePath, "files/").concat(fields.module_id, "/").concat(hash, ".").concat(type); //копирование файла в постоянную папку
+
+                _context.next = 11;
+                return _fsExtra["default"].copy(fields.file.path, savePath);
+
+              case 11:
+                //добавление записи о файле в таблицу
+                arFields = {
+                  size: fields.file.size,
+                  path: savePath,
+                  type: fields.file.type,
+                  url: url,
+                  title: fields.title ? fields.title : fields.file.title,
+                  create_id: fields.create_id
+                };
+                _context.next = 14;
+                return _db.DB.Init.Insert("files", arFields, "id");
+
+              case 14:
+                result = _context.sent;
+                return _context.abrupt("return", result[0].id);
+
+              case 18:
+                _context.prev = 18;
+                _context.t0 = _context["catch"](0);
+                console.log(_context.t0);
+                throw {
+                  err: 3001000,
+                  msg: 'CFile SaveFile'
+                };
+
+              case 22:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee, this, [[0, 18]]);
+      }));
+
+      function SaveFile(_x, _x2) {
+        return _SaveFile.apply(this, arguments);
+      }
+
+      return SaveFile;
+    }() //загрузка файлов
+
+  }, {
+    key: "GetById",
+    value: function () {
+      var _GetById = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(ids) {
+        var result;
+        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                _context2.prev = 0;
+
+                if (ids) {
+                  _context2.next = 3;
+                  break;
+                }
+
+                return _context2.abrupt("return", false);
+
+              case 3:
+                ids = ids.join(',');
+                _context2.next = 6;
+                return _db.DB.Init.Query("SELECT * FROM files WHERE id in (".concat(ids, ")"));
+
+              case 6:
+                result = _context2.sent;
+                return _context2.abrupt("return", result);
+
+              case 10:
+                _context2.prev = 10;
+                _context2.t0 = _context2["catch"](0);
+                console.log(_context2.t0);
+                throw {
+                  err: 3002000,
+                  msg: 'CFile GetById'
+                };
+
+              case 14:
+              case "end":
+                return _context2.stop();
+            }
+          }
+        }, _callee2, null, [[0, 10]]);
+      }));
+
+      function GetById(_x3) {
+        return _GetById.apply(this, arguments);
+      }
+
+      return GetById;
+    }() //удаление информации о файле из базы и сам файл
+
+  }, {
+    key: "Delete",
+    value: function () {
+      var _Delete = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3(id, deleteFile) {
+        var _result, result;
+
+        return regeneratorRuntime.wrap(function _callee3$(_context3) {
+          while (1) {
+            switch (_context3.prev = _context3.next) {
+              case 0:
+                _context3.prev = 0;
+
+                if (!deleteFile) {
+                  _context3.next = 10;
+                  break;
+                }
+
+                _context3.next = 4;
+                return _db.DB.Init.Query("SELECT * FROM files WHERE id=$1", [id]);
+
+              case 4:
+                _result = _context3.sent;
+
+                if (_result[0]) {
+                  _context3.next = 7;
+                  break;
+                }
+
+                return _context3.abrupt("return", false);
+
+              case 7:
+                _context3.next = 9;
+                return _fsExtra["default"].remove("".concat(_result[0].path, "/").concat(_result[0].name));
+
+              case 9:
+                _result = _context3.sent;
+
+              case 10:
+                _context3.next = 12;
+                return _db.DB.Init.Query("DELETE FROM files WHERE id=$1", [id]);
+
+              case 12:
+                result = _context3.sent;
+                return _context3.abrupt("return", true);
+
+              case 16:
+                _context3.prev = 16;
+                _context3.t0 = _context3["catch"](0);
+                console.log(_context3.t0);
+                throw {
+                  err: 3003000,
+                  msg: 'CFile Delete'
+                };
+
+              case 20:
+              case "end":
+                return _context3.stop();
+            }
+          }
+        }, _callee3, null, [[0, 16]]);
+      }));
+
+      function Delete(_x4, _x5) {
+        return _Delete.apply(this, arguments);
+      }
+
+      return Delete;
+    }()
+  }]);
+
+  return _default;
+}();
+
+exports["default"] = _default;
---
+++ build/classes/message.js
@@ -0,0 +1,372 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _db = require("./db");
+
+var _file = _interopRequireDefault(require("./file"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var _default = /*#__PURE__*/function () {
+  function _default() {
+    _classCallCheck(this, _default);
+  }
+
+  _createClass(_default, null, [{
+    key: "Add",
+    //добавить новое видео
+    value: function () {
+      var _Add = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(fields) {
+        var result;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _context.prev = 0;
+                _context.next = 3;
+                return _db.DB.Init.Insert("messages", fields, "ID");
+
+              case 3:
+                result = _context.sent;
+                return _context.abrupt("return", result[0]);
+
+              case 7:
+                _context.prev = 7;
+                _context.t0 = _context["catch"](0);
+                console.log(_context.t0);
+                throw {
+                  err: 5001000,
+                  msg: 'CMessage Add'
+                };
+
+              case 11:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee, null, [[0, 7]]);
+      }));
+
+      function Add(_x) {
+        return _Add.apply(this, arguments);
+      }
+
+      return Add;
+    }() //загрузка
+
+  }, {
+    key: "GetByUserId",
+    value: function () {
+      var _GetByUserId = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(fields) {
+        var sql, result, arMessages, i, messages;
+        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                _context2.prev = 0;
+                sql = "SELECT messages.*,\n\nfrom_user.login as from_user_login,\nfrom_user.personal_photo as from_user_personal_photo,\nfrom_user.personal_gender as from_user_personal_gender,\nfrom_user.name as from_user_name,\n\nto_user.login as to_user_login,\nto_user.personal_photo as to_user_personal_photo,\nto_user.personal_gender as to_user_personal_gender,\nto_user.name as to_user_name\n\nFROM messages \nLEFT JOIN users AS from_user ON messages.from_id=from_user.id \nLEFT JOIN users AS to_user ON messages.to_id=to_user.id \nWHERE (from_id=$1 AND to_id=$2) OR (from_id=$2 AND to_id=$1)";
+                sql += " LIMIT $3 OFFSET $4 ";
+                _context2.next = 5;
+                return _db.DB.Init.Query(sql, [fields.owner_id, fields.user_id, fields.count, fields.offset]);
+
+              case 5:
+                result = _context2.sent;
+                arMessages = []; //массив сообщений уникальных пользователей
+                //уникальность массива
+
+                for (i = 0; i < result.length; i++) {
+                  //добавление новых полей к массиву
+                  messages = {}; //добавление новых полей
+
+                  if (Number(result[i].from_id) === fields.owner_id) {
+                    messages.user_id = Number(result[i].to_user_id);
+                    messages.user_name = result[i].to_user_name;
+                    messages["in"] = false;
+                  } else {
+                    messages.user_id = Number(result[i].from_user_id);
+                    messages.user_name = result[i].from_user_name;
+                    messages["in"] = true;
+                  } //удаление не актуальных полей
+
+
+                  delete result[i].id;
+                  delete result[i].from_user_id;
+                  delete result[i].to_user_id;
+                  arMessages.push(_objectSpread(_objectSpread({}, result[i]), messages));
+                }
+
+                return _context2.abrupt("return", arMessages);
+
+              case 11:
+                _context2.prev = 11;
+                _context2.t0 = _context2["catch"](0);
+                console.log(_context2.t0);
+                throw {
+                  err: 5002000,
+                  msg: 'CMessage GetUserId'
+                };
+
+              case 15:
+              case "end":
+                return _context2.stop();
+            }
+          }
+        }, _callee2, null, [[0, 11]]);
+      }));
+
+      function GetByUserId(_x2) {
+        return _GetByUserId.apply(this, arguments);
+      }
+
+      return GetByUserId;
+    }()
+  }, {
+    key: "Get",
+    value: function () {
+      var _Get = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3(fields) {
+        var sql, outMes, inMes, result, arMessages, CheckContinue, i, messages, j;
+        return regeneratorRuntime.wrap(function _callee3$(_context3) {
+          while (1) {
+            switch (_context3.prev = _context3.next) {
+              case 0:
+                _context3.prev = 0;
+                //ИСХОДЯЩИЕ
+                sql = "SELECT messages.*,\n\nfrom_user.login as from_user_login,\nfrom_user.personal_photo as from_user_personal_photo,\nfrom_user.personal_gender as from_user_personal_gender,\nfrom_user.name as from_user_name,\n\nto_user.login as to_user_login,\nto_user.personal_photo as to_user_personal_photo,\nto_user.personal_gender as to_user_personal_gender,\nto_user.name as to_user_name\n\nFROM messages \nLEFT JOIN users AS from_user ON messages.from_id=from_user.id \nLEFT JOIN users AS to_user ON messages.to_id=to_user.id \n\nWHERE messages.from_id=$1 AND (messages.to_id, messages.date_create) in (\nSELECT to_id, max(date_create)\nFROM messages \nWHERE from_id=$1\nGROUP BY to_id)";
+                sql += " LIMIT $2 OFFSET $3 ";
+                _context3.next = 5;
+                return _db.DB.Init.Query(sql, [fields.from_id, fields.count, fields.offset]);
+
+              case 5:
+                outMes = _context3.sent;
+                //ВХОДЯЩИЕ
+                sql = "SELECT messages.*,\n\nfrom_user.login as from_user_login,\nfrom_user.personal_photo as from_user_personal_photo,\nfrom_user.personal_gender as from_user_personal_gender,\nfrom_user.name as from_user_name,\n\nto_user.login as to_user_login,\nto_user.personal_photo as to_user_personal_photo,\nto_user.personal_gender as to_user_personal_gender,\nto_user.name as to_user_name\n\nFROM messages \nLEFT JOIN users AS from_user ON messages.from_id=from_user.id \nLEFT JOIN users AS to_user ON messages.to_id=to_user.id \n\nWHERE messages.to_id=$1 AND (messages.from_id, messages.date_create) in (\nSELECT from_id, max(date_create)\nFROM messages \nWHERE to_id=$1\nGROUP BY from_id)";
+                sql += " LIMIT $2 OFFSET $3 ";
+                _context3.next = 10;
+                return _db.DB.Init.Query(sql, [fields.from_id, fields.count, fields.offset]);
+
+              case 10:
+                inMes = _context3.sent;
+                //объединение входящих и исходящих сообщений
+                result = outMes.concat(inMes); //ОБЪЕДИНЕНИЕ ВХОДЯЩИХ и ИСХОДЯЩИХ сообщений
+
+                arMessages = []; //массив сообщений уникальных пользователей
+
+                CheckContinue = false; //выйти из цикла до сохранения элемента массива
+                //уникальность массива
+
+                i = 0;
+
+              case 15:
+                if (!(i < result.length)) {
+                  _context3.next = 44;
+                  break;
+                }
+
+                CheckContinue = false; //сброс чека / выход из цикла
+                //добавление новых полей к массиву
+
+                messages = {}; //добавление новых полей
+
+                if (Number(result[i].from_user_id) === fields.from_id) {
+                  messages.user_id = Number(result[i].to_user_id);
+                  messages.user_name = result[i].to_user_name;
+                  messages.user_personal_photo = result[i].to_user_personal_photo;
+                  messages.user_personal_gender = result[i].to_user_personal_gender;
+                  messages["in"] = false;
+                } else {
+                  messages.user_id = Number(result[i].from_user_id);
+                  messages.user_name = result[i].from_user_name;
+                  messages.user_personal_photo = result[i].from_user_personal_photo;
+                  messages.user_personal_gender = result[i].from_user_personal_gender;
+                  messages["in"] = true;
+                } //удаление не актуальных полей
+
+
+                delete result[i].id;
+                delete result[i].from_user_id;
+                delete result[i].from_user_login;
+                delete result[i].from_user_name;
+                delete result[i].from_user_personal_photo;
+                delete result[i].from_user_personal_gender;
+                delete result[i].to_user_id;
+                delete result[i].to_user_login;
+                delete result[i].to_user_name;
+                delete result[i].to_user_personal_photo;
+                delete result[i].to_user_personal_gender; //проходим по массиву еще раз и ищем такой же
+
+                j = 0;
+
+              case 31:
+                if (!(j < arMessages.length)) {
+                  _context3.next = 38;
+                  break;
+                }
+
+                if (!(messages.user_id === arMessages[j].user_id)) {
+                  _context3.next = 35;
+                  break;
+                }
+
+                CheckContinue = true;
+                return _context3.abrupt("break", 38);
+
+              case 35:
+                j++;
+                _context3.next = 31;
+                break;
+
+              case 38:
+                if (!CheckContinue) {
+                  _context3.next = 40;
+                  break;
+                }
+
+                return _context3.abrupt("continue", 41);
+
+              case 40:
+                arMessages.push(_objectSpread(_objectSpread({}, result[i]), messages));
+
+              case 41:
+                i++;
+                _context3.next = 15;
+                break;
+
+              case 44:
+                return _context3.abrupt("return", arMessages);
+
+              case 47:
+                _context3.prev = 47;
+                _context3.t0 = _context3["catch"](0);
+                console.log(_context3.t0);
+                throw {
+                  err: 5003000,
+                  msg: 'CMessage Get'
+                };
+
+              case 51:
+              case "end":
+                return _context3.stop();
+            }
+          }
+        }, _callee3, null, [[0, 47]]);
+      }));
+
+      function Get(_x3) {
+        return _Get.apply(this, arguments);
+      }
+
+      return Get;
+    }() //добавить новое видео
+
+  }, {
+    key: "MarkAsReadAll",
+    value: function () {
+      var _MarkAsReadAll = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee4(fields) {
+        var sql, result;
+        return regeneratorRuntime.wrap(function _callee4$(_context4) {
+          while (1) {
+            switch (_context4.prev = _context4.next) {
+              case 0:
+                _context4.prev = 0;
+                sql = "UPDATE messages SET read = true WHERE from_user_id=".concat(fields.from_id, " AND to_user_id=").concat(fields.to_id, " AND id < ").concat(fields.start_message_id);
+                console.log(sql);
+                _context4.next = 5;
+                return _db.DB.Init.Query(sql);
+
+              case 5:
+                result = _context4.sent;
+                _context4.next = 12;
+                break;
+
+              case 8:
+                _context4.prev = 8;
+                _context4.t0 = _context4["catch"](0);
+                console.log(_context4.t0);
+                throw {
+                  err: 5004000,
+                  msg: 'CMessage MarkAsReadAll'
+                };
+
+              case 12:
+              case "end":
+                return _context4.stop();
+            }
+          }
+        }, _callee4, null, [[0, 8]]);
+      }));
+
+      function MarkAsReadAll(_x4) {
+        return _MarkAsReadAll.apply(this, arguments);
+      }
+
+      return MarkAsReadAll;
+    }() //добавить новое видео
+
+  }, {
+    key: "MarkAsRead",
+    value: function () {
+      var _MarkAsRead = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee5(fields) {
+        var sql, result;
+        return regeneratorRuntime.wrap(function _callee5$(_context5) {
+          while (1) {
+            switch (_context5.prev = _context5.next) {
+              case 0:
+                _context5.prev = 0;
+                sql = "UPDATE messages SET read = true WHERE from_user_id=".concat(fields.from_user_id, " AND id in (").concat(fields.message_ids, ")");
+                _context5.next = 4;
+                return _db.DB.Init.Query(sql);
+
+              case 4:
+                result = _context5.sent;
+                _context5.next = 11;
+                break;
+
+              case 7:
+                _context5.prev = 7;
+                _context5.t0 = _context5["catch"](0);
+                console.log(_context5.t0);
+                throw {
+                  err: 5005000,
+                  msg: 'CMessage Add'
+                };
+
+              case 11:
+              case "end":
+                return _context5.stop();
+            }
+          }
+        }, _callee5, null, [[0, 7]]);
+      }));
+
+      function MarkAsRead(_x5) {
+        return _MarkAsRead.apply(this, arguments);
+      }
+
+      return MarkAsRead;
+    }()
+  }]);
+
+  return _default;
+}();
+
+exports["default"] = _default;
