94e99d19faef64bf78ff01ad16e74d87f6a43f9f
stronggeniusa@gmail.com <stronggeniusa@gmail.com>
2021-08-30 14:35:54 +0700

    Друзья доработаны

--- src/classes/friend.js
+++ src/classes/friend.js
@@ -5,12 +5,122 @@ export default class {
 
     static async Add ( fields ) {
         try {
-            //запись
-            let result = await DB.Init.Insert(`${DB.Init.TablePrefix}friend`, fields, `ID`)
-            return result[0]
+            let sql = `SELECT * FROM ${DB.Init.TablePrefix}friend WHERE
+                                ((user_id=${fields.user_id} AND friend_id=${fields.friend_id}) OR
+                                 (user_id=${fields.friend_id} AND friend_id=${fields.user_id}))
+                            AND allowed is null`
+
+            console.log(sql)
+            let resultFriend = await DB.Init.Query(sql)
+
+            console.log(resultFriend)
+
+            if (!resultFriend.length) {
+                let resultAdd = await DB.Init.Insert(`${DB.Init.TablePrefix}friend`, fields, `ID`)
+                return resultAdd[0]
+            }
+
+            //заявка входящая
+            if (Number (resultFriend[0].friend_id) === Number(fields.user_id)) {
+                //просматриваем и принимаем
+                let newField = {
+                    viewed: true,
+                    allowed: true
+                }
+
+                let resultEdit = await DB.Init.Update (`${DB.Init.TablePrefix}friend`, newField, {id: resultFriend[0].id},`id`)
+                return resultEdit[0]
+            }
+
+            return false
         } catch (err) {
             console.log(err)
             throw ({err: 4001000, msg: 'CFriend Add'})
+        }
+    }
+
+    static async Delete ( fields ) {
+        try {
+            let sql = `SELECT * FROM ${DB.Init.TablePrefix}friend WHERE
+                                ((user_id=${fields.user_id} AND friend_id=${fields.friend_id}) OR
+                                 (user_id=${fields.friend_id} AND friend_id=${fields.user_id}))`
+
+            console.log(sql)
+            let resultFriend = await DB.Init.Query(sql)
+
+            console.log(resultFriend)
+
+            //нечего удалять
+            if (!resultFriend.length)
+                return false
+
+            resultFriend = resultFriend[0]
+
+            //заявка входящая / in
+            if (Number (resultFriend.friend_id) === Number(fields.user_id)) {
+                let newField = {
+                    viewed: true,
+                    allowed: null
+                }
+                await DB.Init.Update (`${DB.Init.TablePrefix}friend`, newField, {id: resultFriend.id},`id`)
+
+                return true
+            }
+
+            //заявка исходящая / out
+            if (Number (resultFriend.user_id) === Number(fields.user_id)) {
+                let sql = `DELETE FROM ${DB.Init.TablePrefix}friend WHERE user_id=${fields.user_id} AND friend_id=${fields.friend_id}`
+                await DB.Init.Query(sql)
+
+                return true
+            }
+
+            return false
+
+            return false
+        } catch (err) {
+            console.log(err)
+            throw ({err: 4001000, msg: 'CFriend Add'})
+        }
+    }
+
+    static async Status ( fields ) {
+        try {
+            let sql = `SELECT * FROM ${DB.Init.TablePrefix}friend WHERE
+                                ((user_id=${fields.user_id} AND friend_id=${fields.friend_id}) OR
+                                 (user_id=${fields.friend_id} AND friend_id=${fields.user_id}))`
+
+            console.log(sql)
+            let resultFriend = await DB.Init.Query(sql)
+
+            console.log(resultFriend)
+
+            //нет заявки
+            if (!resultFriend.length)
+                return 'none'
+
+            resultFriend = resultFriend[0]
+
+            //друг
+            if (resultFriend.allowed === true)
+                return 'friend'
+
+            //заявка входящая
+            if ((Number (resultFriend.friend_id) === Number(fields.user_id)) && (resultFriend.viewed === true))
+                return 'viewed'
+
+            //заявка входящая
+            if (Number (resultFriend.friend_id) === Number(fields.user_id))
+                return 'in'
+
+            //заявка исходящая
+            if (Number (resultFriend.user_id) === Number(fields.user_id))
+                return 'out'
+
+            return false
+        } catch (err) {
+            console.log(err)
+            throw ({err: 4001000, msg: 'CFriend Status'})
         }
     }
 
