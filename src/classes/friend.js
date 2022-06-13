ab4e3a496403f174b4937e8de59f3b72637120a8
stronggeniusa@gmail.com <stronggeniusa@gmail.com>
2022-06-13 21:21:59 +0700

    Друзья Get

--- src/classes/friend.js
+++ src/classes/friend.js
@@ -47,40 +47,6 @@ export class CFriend {
             }
 
             return false
-
-            /*
-            let sql = `SELECT * FROM ${DB.Init.TablePrefix}friend WHERE
-                                ((user_id=${fields.user_id} AND friend_id=${fields.friend_id}) OR
-                                 (user_id=${fields.friend_id} AND friend_id=${fields.user_id}))
-                            AND allowed is null`
-
-            console.log(sql)
-            let resultFriend = await DB.Init.Query(sql)*/
-
-            //console.log(resultFriend)
-
-            /*
-                        if (!resultFriend.length) {
-                let resultAdd = await DB.Init.Insert(`${DB.Init.TablePrefix}friend`, fields, `ID`)
-                return resultAdd[0]
-            }
-             */
-/*
-
-
-            //заявка входящая
-            if (Number (resultFriend[0].friend_id) === Number(fields.user_id)) {
-                //просматриваем и принимаем
-                let newField = {
-                    viewed: true,
-                    allowed: true
-                }
-
-                let resultEdit = await DB.Init.Update (`${DB.Init.TablePrefix}friend`, newField, {id: resultFriend[0].id},`id`)
-                return resultEdit[0]
-            }
-
-            return false*/
         } catch (err) {
             console.log(err)
             throw ({code: 4001000, msg: 'CFriend Add'})
@@ -117,45 +83,6 @@ export class CFriend {
 
             collection.deleteOne({_id: selectFriend._id})
             return true
-
-            /*
-            let sql = `SELECT * FROM ${DB.Init.TablePrefix}friend WHERE
-                                ((user_id=${fields.user_id} AND friend_id=${fields.friend_id}) OR
-                                 (user_id=${fields.friend_id} AND friend_id=${fields.user_id}))`
-
-            console.log(sql)
-            let resultFriend = await DB.Init.Query(sql)
-
-            console.log(resultFriend)
-
-            //нечего удалять
-            if (!resultFriend.length)
-                return false
-
-            resultFriend = resultFriend[0]
-
-            //заявка входящая / in
-            if (Number (resultFriend.friend_id) === Number(fields.user_id)) {
-                let newField = {
-                    viewed: true,
-                    allowed: null
-                }
-                await DB.Init.Update (`${DB.Init.TablePrefix}friend`, newField, {id: resultFriend.id},`id`)
-
-                return true
-            }
-
-            //заявка исходящая / out
-            if (Number (resultFriend.user_id) === Number(fields.user_id)) {
-                let sql = `DELETE FROM ${DB.Init.TablePrefix}friend WHERE user_id=${fields.user_id} AND friend_id=${fields.friend_id}`
-                await DB.Init.Query(sql)
-
-                return true
-            }
-
-            return false*/
-
-
         } catch (err) {
             console.log(err)
             throw ({code: 4001000, msg: 'CFriend Add'})
@@ -230,6 +157,72 @@ export class CFriend {
     //загрузка
     static async Get ( fields ) {
         try {
+            let collection = DB.Client.collection('friend')
+            fields.user_id = new DB().ObjectID(fields.user_id)
+
+            let arAggregate = [{
+                $match: {
+                    user_id: fields.user_id
+                },
+            },{
+                $lookup:
+                    {
+                        from: 'user',
+                        localField: 'user_id',
+                        foreignField: '_id',
+                        as: '_user_id',
+                        pipeline: [
+                            { $lookup:
+                                    {
+                                        from: 'file',
+                                        localField: 'photo',
+                                        foreignField: '_id',
+                                        as: '_photo'
+                                    }
+                            },
+                            {
+                                $unwind:
+                                    {
+                                        path: '$_photo',
+                                        preserveNullAndEmptyArrays: true
+                                    }
+                            }
+                        ]
+                    },
+            },{
+                $lookup:
+                    {
+                        from: 'user',
+                        localField: 'friend_id',
+                        foreignField: '_id',
+                        as: '_friend_id',
+                        pipeline: [
+                            { $lookup:
+                                    {
+                                        from: 'file',
+                                        localField: 'photo',
+                                        foreignField: '_id',
+                                        as: '_photo'
+                                    }
+                            },
+                            {
+                                $unwind:
+                                    {
+                                        path: '$_photo',
+                                        preserveNullAndEmptyArrays: true
+                                    }
+                            }
+                        ]
+                    },
+            },{
+                $sort: {
+                    _id: -1
+                }
+            }]
+            let result = await collection.aggregate(arAggregate).limit(fields.count+fields.offset).skip(fields.offset).toArray();
+            return result
+
+            /*
 
             let sql = `SELECT * FROM ${DB.Init.TablePrefix}friend WHERE (user_id=${fields.user_id} OR friend_id=${fields.user_id}) AND allowed=true ORDER BY id DESC`
             sql += ` LIMIT $1 OFFSET $2 `
@@ -275,7 +268,7 @@ export class CFriend {
                 return newField;
             }));
 
-            return arUsers
+            return arUsers*/
         } catch (err) {
             console.log(err)
             throw ({code: 6003000, msg: 'CFriend Get'})
@@ -285,18 +278,38 @@ export class CFriend {
     //количество
     static async GetCount ( fields ) {
         try {
+            fields.from_id = new DB().ObjectID(fields.from_id)
+
+            let collection = DB.Client.collection('friend')
+
+            let Aggregate = [
+                {
+                    $match: {
+                        user_id: fields.user_id
+                    },
+                },{
+                    $count: 'count'
+                }
+            ]
+
+            let result = await collection.aggregate(Aggregate).toArray();
+            if (!result.length) return 0
+            return result[0].count
+
+            /*
             let sql = `SELECT COUNT(*) FROM ${DB.Init.TablePrefix}friend WHERE (user_id=${fields.user_id} OR friend_id=${fields.user_id}) AND allowed=true`
             //let sql = `SELECT COUNT(*) FROM ${DB.Init.TablePrefix}post WHERE owner_id=${fields.owner_id}`
 
             let result = await DB.Init.Query(sql)
 
-            return Number (result[0].count)
+            return Number (result[0].count)*/
         } catch (err) {
             console.log(err)
             throw ({code: 6004000, msg: 'CFriend GetCount'})
         }
     }
 
+    /*
     static async Accept ( fields, where ) {
         try {
             //просмотренно
@@ -310,5 +323,5 @@ export class CFriend {
             console.log(err)
             throw ({code: 4001000, msg: 'CFriend Accept'})
         }
-    }
+    }*/
 }
