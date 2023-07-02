9602b90dc56832250e81232382fe1f4ce7788140
stronggeniusa <stronggeniusa@gmail.com>
2023-07-02 17:21:45 +0700

    Обновлены зависимости

--- src/classes/auth.js
+++ src/classes/auth.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import * as crypto from "crypto"
 import { DB } from "./db"
 import { CUser } from './user'
--- src/classes/user.js
+++ src/classes/user.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import crypto from "crypto";
 import { DB } from "./db"
 import { Store } from "../store"
