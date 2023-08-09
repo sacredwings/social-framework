5d1d455871232dd06c1ae80efb921966f168c581
stronggeniusa <stronggeniusa@gmail.com>
2023-08-09 22:46:58 +0700

    Загрузка постов

--- src/classes/post.js
+++ src/classes/post.js
@@ -211,7 +211,7 @@ export class CPost {
             if (fields.q) arAggregate[0].$match.$text = {}
             if (fields.q) arAggregate[0].$match.$text.$search = fields.q
 
-            if ((fields.to_user_id) && (!fields.to_group_id)) arAggregate[0].$match.to_user_id = fields.to_user_id
+            if ((fields.to_user_id) && (!fields.to_group_id)) arAggregate[0].$match.from_id = fields.to_user_id
             if (fields.to_group_id) arAggregate[0].$match.to_group_id = fields.to_group_id
 
             let result = await collection.aggregate(arAggregate).limit(fields.count+fields.offset).skip(fields.offset).toArray();
