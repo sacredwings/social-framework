c92e972f9c5671210ab5a677c05255fb9c108c47
stronggeniusa <stronggeniusa@gmail.com>
2023-10-17 20:34:26 +0700

    Класс файлов переписан

--- src/classes/auth.js
+++ src/classes/auth.js
@@ -2,209 +2,314 @@ import bcrypt from "bcryptjs"
 import * as crypto from "crypto"
 import { DB } from "./db"
 import { CUser } from './user'
+import { CFile } from './file'
 import axios from "axios"
 import { Store } from "../store"
 
 export class CAuth {
 
-    static async Login ( fields ) {
+    //авторизация по полю и паролю
+    //* password
+    //* ip
+    //* device
+    static async LoginByField ({password, ...value}) {
         try {
-            const mongoClient = Store.GetMongoClient()
-            //поиск пользователя по логину
-            let user = await CUser.GetByLogin(fields.login);
+            //поиск пользователя
+            let user = await CUser.GetByField({value})
             if (!user)
-                throw ({code: 1001001, msg: 'Неверный логин'});
+                throw ({code: 1001001, msg: 'Пользователь не найден'})
 
             //сравнение паролей
-            let match = await bcrypt.compare(fields.password, user.password);
+            let match = await bcrypt.compare(password, user.password)
             if (!match)
-                throw ({code: 1001002, msg: 'Неверный пароль'});
+                throw ({code: 1001002, msg: 'Неверный пароль'})
 
-            let token = await this.AddToken(user._id, fields.ip, fields.browser);
+            //новый токен
+            let token = await this.TokenAdd({
+                userId: user._id,
+                ip: value.ip,
+                device: value.device
+            })
             if (!token)
-                throw ({code: 1001003, msg: 'Токен не создан'});
+                throw ({code: 1001003, msg: 'Токен не создан'})
 
-            return {tid: token._id, token: token.token, _id: user._id, login: user.login}
+            return {tid: token._id, tkey: token.key, _id: user._id, login: user.login}
 
         } catch (err) {
-            throw ({...{code: 1001000, msg: 'CAuth Login'}, ...err});
+            throw ({...{code: 1001000, msg: 'CAuth Login'}, ...err})
         }
     }
 
-    static async GetById ( ids ) {
-        try {
-            const mongoClient = Store.GetMongoClient()
-            ids = new DB().arObjectID(ids)
-
-            let collection = mongoClient.collection('auth');
-            let result = await collection.find({_id: { $in: ids}}).toArray()
-
-            if (result)
-                return result
-
-            return false
-        } catch (err) {
-            console.log(err)
-            throw ({code: 1003000, msg: 'CAuth GetById'})
-        }
-    }
-
-    static async AddToken ( userId, ip, browser ) {
+    //Добавление токена
+    //* password
+    //* ip
+    //* device
+    static async TokenAdd ({userId, ip, device}) {
         try {
             const mongoClient = Store.GetMongoClient()
             let collection = mongoClient.collection('auth')
 
-            //создаем hash /нужно поменять на дату
-            let hash = new Date().toString()
+            //создаем hash
+            let hash = new Date().toString() //ключ
             hash = crypto.createHash('md5').update(hash).digest("hex")
 
             //подготовка полей
             let arFields = {
-                token: hash,
                 user_id: userId,
-                ip: ip,
-                browser: browser
+                key: hash,
+
+                ip: (ip) ? ip : null,
+                device: (device) ? device : null,
             };
-
-            let result = await collection.insertOne(arFields)
-
+            await collection.insertOne(arFields)
             return arFields
-            //console.log(result)
-            //console.log(arFields)
-
-            //запись
-            //let result = await DB.Init.Insert(`${DB.Init.TablePrefix}token`, arFields, `id, token`)
-            //return result[0]
-
         } catch (err) {
             console.log(err)
-            throw ({code: 1004000, msg: 'CAuth AddToken'})
+            throw ({code: 1004000, msg: 'CAuth TokenAdd'})
         }
     }
 
-    static async Auth ( fields ) {
+    //Проверяет авторизован ли посетитель
+    //* tid
+    //* tkey
+    static async TokenGetByIdKey ({tid, tkey}) {
         try {
-            //существование id и токена авторизации
-            if ((!fields) || ((!fields.tid) || (!fields.token)))
-                return null
+            const mongoClient = Store.GetMongoClient()
+            tid = new DB().ObjectID(tid)
 
             //поиск ключа
-            let user = await this.GetById([fields.tid])
-            if (!user.length)
-                return null
+            let collection = mongoClient.collection('auth');
+            let token = await collection.findOne({_id: tid, key: tkey})
 
-            user = user[0] //из массива берем первый
-
-            if (fields.token !== user.token)
-                return null
-
-            return user.user_id
+            if (!token) return false
+            return token.user_id
         } catch (err) {
             console.log(err)
-            throw ({code: 1004000, msg: 'CAuth Access'})
+            throw ({code: 1004000, msg: 'CAuth IsAuthorized'})
         }
     }
 
-    static async AuthVK ({client_id, client_secret, redirect_uri, code, browser}) {
+    //Авторизация ВК
+    static async AuthVK ({userId, clientId, clientSecret, redirectUri, code, device, bucketName}) {
         try {
-            let profile;
-            let url = `https://oauth.vk.com/access_token?client_id=${client_id}&client_secret=${client_secret}&redirect_uri=${redirect_uri}&code=${code}`;
+            //АВТОРИЗОВАН - поиск пользователя по ID / нужны все поля для дальнейших проверок
+            let arUser = false
+            if (userId) {
+                arUser = await CUser.GetById([userId])
+                arUser = arUser[0]
+            }
 
-            console.log(url)
-            let infoVk;
+            //авторизован и есть id vk - ошибка
+            if ((arUser) && (arUser.vk_id))
+                throw ({code: 30080002, msg: 'ВК уже привязан'})
+
+            //основной запрос в вк
+            let vkInfo = null
             try {
-                infoVk = await axios({
+                let urlParam = {
+                    client_id: clientId,
+                    client_secret: clientSecret,
+                    redirect_uri: redirectUri,
+                    code: code
+                }
+                let url = `https://oauth.vk.com/access_token?${new URLSearchParams(urlParam).toString()}`
+                vkInfo = await axios({
                     method: 'get',
                     url: url,
-                    headers: {'User-Agent': browser}
-                });
-
+                    headers: {'User-Agent': device}
+                })
+                vkInfo = vkInfo.data
             } catch (err) {
-                console.log(err.response.data)
-                throw ({...{err: 30080001, msg: 'VK возвращает ошибку'}});
+                throw ({...{code: 30080001, msg: 'ВК: ошибка в oauth.vk.com'}});
+            }
+            console.log(vkInfo)
+
+            //запрос информации о пользователе
+            let vkUser;
+            try {
+                let urlParam = {
+                    user_id: vkInfo.user_id,
+                    fields: 'bdate,photo_max_orig,sex',
+                    access_token: vkInfo.access_token,
+                    v: '5.131'
+                }
+                let url = `https://api.vk.com/method/users.get?${new URLSearchParams(urlParam).toString()}`
+                vkUser = await axios({
+                    method: 'get',
+                    url: url,
+                    headers: {'User-Agent': device}
+                });
+                vkUser = vkUser.data.response[0]
+            } catch (err) {
+                throw ({code: 30080004, msg: 'ВК: ошибка в методе users.get'});
+            }
+            console.log(vkUser)
+
+            let userAuth = false
+            //Прежде чем, чтото делать, ищем пользователя с совпадениями
+            let arSearchUsers = {
+                id: false,
+                email: false,
+                phone: false
             }
 
-            if ((!infoVk.data) || (!infoVk.data.user_id) || (!infoVk.data.email) || (!infoVk.data.access_token))
-                throw ({err: 30080002, msg: 'Не полный ответ от VK'});
+            if (vkInfo.user_id)
+                arSearchUsers.id = await CUser.GetByField({
+                    vk_id: vkInfo.user_id
+                })
+            if (vkInfo.email)
+                arSearchUsers.email = await CUser.GetByField({
+                    email: vkInfo.email
+                })
+            if (vkInfo.phone_number)
+                arSearchUsers.phone = await CUser.GetByField({
+                    phone: vkInfo.phone_number
+                })
 
-            //почта ВК в нижний регистр
-            infoVk.data.email = infoVk.data.email.toLowerCase();
-
-            //поиск профиля в боте / или по id вк или по email
-            let arVkProfile = await CUser.GetUserByVk({
-                vk_id: infoVk.data.user_id,
-                email: infoVk.data.email
-            }, )
-
-            //профиля нет /нужно создать
-            if (!arVkProfile) {
-                console.log('профиля нет /нужно создать');
-
-                //генерация пароля
-                let password = getRandomInt(10000000, 99999999);
-                const saltRounds = 10;
-                let passwordSalt = await bcrypt.genSalt(saltRounds);
-                password = await bcrypt.hash(`${password}`, passwordSalt);
-
-                let vkUser;
-                try {
-                    let url = `https://api.vk.com/method/users.get?user_id=${infoVk.data.user_id}`;
-                    url += `&access_token=${infoVk.data.access_token}&v=5.131`;
-                    vkUser = await axios({
-                        method: 'get',
-                        url: url,
-                        headers: {'User-Agent': browser}
-                    });
-                } catch (err) {
-                    throw ({err: 30080004, msg: 'ВК: ошибка в методе users.get'});
+            //авторизован
+            if (arUser) {
+                //если поля у пользователей не найдены - можно привязать к текущему
+                if ((!arSearchUsers.id) && (!arSearchUsers.email) && (!arSearchUsers.phone)) {
+                    let arFields = {
+                        vk_id: vkInfo.user_id
+                    }
+                    if (!arUser.email) arFields.email = vkInfo.email
+                    if (!arUser.phone) arFields.phone = vkInfo.phone_number
+                    await CUser.Edit(arUser._id, arFields)
+                    userAuth = arUser
                 }
 
-                if ((!vkUser.data.response.length) || (!vkUser.data.response[0].first_name) && (!vkUser.data.response[0].last_name))
-                    throw ({err: 30080005, msg: 'ВК: ошибка в методе users.get'});
-
-                //profile = await modelsProfile.addUser(infoVk.data.email, password, vkUser.data.response[0].first_name, vkUser.data.response[0].last_name, null, value.ref, infoVk.data.user_id);
-
-                //список
-                let arFields = {
-                    email: infoVk.data.email,
-                    login: infoVk.data.email,
-                    password: password,
-                    first_name: vkUser.data.response[0].first_name,
-                    last_name: vkUser.data.response[0].last_name
+                if ((arSearchUsers.id) || (arSearchUsers.email) || (arSearchUsers.phone)) {
+                    throw ({code: 30080004, msg: 'ВК уже был привязан'})
                 }
-                profile = await CUser.Add ( arFields )
+
             }
 
-            //профиль не привязан /нужно привязать по почте
-            if ((arVkProfile) && (!arVkProfile.vk_id) && (arVkProfile.email)) {
-                console.log('профиль есть /нужно привязать id')
+            //не авторизован
+            if (!arUser) {
+                //если поля у пользователей не найдены - создаем пользователя
+                if ((!arSearchUsers.id) && (!arSearchUsers.email) && (!arSearchUsers.phone)) {
+                    let arFields = {
+                        login: `vk_${vkInfo.user_id}`,
+                        vk_id: vkInfo.user_id,
+                    }
+                    if (vkInfo.email) arFields.email = vkInfo.email
+                    if (vkInfo.phone_number) arFields.phone = vkInfo.phone_number
 
-                await CUser.Edit(arVkProfile._id, {vk_id: infoVk.data.user_id})
+                    if (vkUser.first_name) arFields.first_name = vkUser.first_name
+                    if (vkUser.last_name) arFields.last_name = vkUser.last_name
+
+                    userAuth = await CUser.Add(arFields)
+                }
+                if ((arSearchUsers.email) || (arSearchUsers.phone)) {
+                    let searchUser = false
+                    if (arSearchUsers.phone) searchUser = arSearchUsers.phone
+                    if (arSearchUsers.email) searchUser = arSearchUsers.email
+
+                    let arFields = {
+                        vk_id: vkInfo.user_id
+                    }
+                    if (!searchUser.email) arFields.email = vkInfo.email
+                    if ((!searchUser.phone) && (vkInfo.phone_number)) arFields.phone = vkInfo.phone_number
+                    await CUser.Edit(searchUser._id, arFields)
+                    userAuth = searchUser
+                }
+                if (arSearchUsers.id) userAuth = arSearchUsers.id
             }
 
-            if (!profile) profile = arVkProfile
+            //авторизация
+            let token = await CAuth.TokenAdd({
+                userId: userAuth._id
+            })
 
-            //создаем авторизацию
-            let token = await this.AddToken(profile._id, null, browser);
-            if (!token)
-                throw ({code: 1001003, msg: 'Токен не создан'});
+            return {tid: token._id, tkey: token.key, _id: userAuth._id, login: userAuth.login}
 
-            return {tid: token._id, token: token.token, _id: profile._id, login: profile.login}
+        } catch (err) {
+            throw ({...{err: 30080000, msg: 'Создание запроса на регистрацию нового пользователя'}, ...err});
+        }
+    }
 
-            //профиль привязан /нужна авторизация
-            /*if ((arProfile) && (arProfile.vk_id)) {
-                console.log('профиль есть /авторизирую');
+    static async Telegram ({userId, telegramToken, telegram, bucketName}) {
+        try {
+            const mongoClient = Store.GetMongoClient()
+            let collectionUser = mongoClient.collection('user')
 
-            }*/
+            //проверка хеша авторизации
+            if (!await checkTgAuth(telegramToken, telegram)) throw ({code: 999, msg: 'Не верная авторизация'})
 
-            /*
-            let token = await modelsAuth.authorization(profile.id, value.ip, value.browser);
-            if (!token)
-                throw ({err: 30080006, msg: 'Токен не создан'});
+            //АВТОРИЗОВАН - поиск пользователя по ID / нужны все поля для дальнейших проверок
+            let arUser = false
+            if (userId) {
+                arUser = await CUser.GetById([userId])
+                arUser = arUser[0]
+            }
 
-            return {...token, ...{login: infoVk.data.email}};*/
+            //поиск среди пользователей
+            let arSearchUser = await CUser.GetByField({
+                tg_id: telegram.id
+            })
 
-            //return profile
+            let userAuth = false
+
+            //авторизован
+            if (arUser) {
+                //если поля у пользователей не найдены - можно привязать к текущему
+                if (!arSearchUser) {
+                    let arFields = {
+                        tg_id: telegram.id,
+                    }
+
+                    //у пользователя фото нет, а в телеграме есть
+                    if ((!arUser.photo_id) && (telegram.photo_url)) {
+                        let rsFile = await CFile.Upload({
+                            module: 'user',
+                            fileUrl: telegram.photo_url,
+                            fromId: userId,
+                            bucketName: bucketName
+                        })
+                        arFields.photo_id = rsFile._id
+                    }
+
+                    await CUser.Edit(arUser._id, arFields)
+                    userAuth = arUser
+                } else {
+                    throw ({code: 30080004, msg: 'TG уже привязан'})
+                }
+            }
+
+            //не авторизован
+            if (!arUser) {
+                //если поля у пользователей не найдены - создаем пользователя
+                if (!arSearchUser) {
+                    let arFields = {
+                        login: 'tg_' + telegram.username,
+                        tg_id: telegram.id,
+                        first_name: telegram.first_name,
+                    }
+
+                    userAuth = await CUser.Add(arFields)
+
+                    //загрузка фото если оно есть
+                    if (telegram.photo_url) {
+                        let rsFile = await CFile.Upload({
+                            module: 'user',
+                            fileUrl: telegram.photo_url,
+                            fromId: userId,
+                            bucketName: bucketName
+                        })
+                        await CUser.Edit(userAuth._id, {photo_id: rsFile._id})
+                    }
+                }
+
+                if (arSearchUser) userAuth = arSearchUser
+            }
+
+            //авторизация
+            let token = await CAuth.TokenAdd({
+                userId: userAuth._id
+            })
+
+            return {tid: token._id, tkey: token.key, _id: userAuth._id, login: userAuth.login}
 
         } catch (err) {
             throw ({...{err: 30080000, msg: 'Создание запроса на регистрацию нового пользователя'}, ...err});
@@ -212,8 +317,24 @@ export class CAuth {
     }
 }
 
+/*
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}*/
+
+async function checkTgAuth(token, { hash, ...userData }) {
+    const secretKey = crypto.createHash('sha256')
+        .update(token)
+        .digest();
+    const dataCheckString = Object.keys(userData)
+        .sort()
+        .map(key => (`${key}=${userData[key]}`))
+        .join('\n');
+    const hmac = crypto.createHmac('sha256', secretKey)
+        .update(dataCheckString)
+        .digest('hex');
+
+    return hmac === hash;
 }
