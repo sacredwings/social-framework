e8a4764062fe5920e6ee4952fdc905287aa82d45
stronggeniusa@gmail.com <stronggeniusa@gmail.com>
2024-09-18 21:26:36 +0700

    Исправлен щетчик комментарий

--- src/classes/comment.ts
+++ src/classes/comment.ts
@@ -950,37 +950,22 @@ export class CComment {
 
 async function count ({from_id, to_user_id, to_group_id, collectionName}) {
     let mongoClient = Store.GetMongoClient()
-
-    let collectionUser = mongoClient.collection('user')
-    let collectionGroup = mongoClient.collection('group')
     let collection = mongoClient.collection(collectionName)
 
     if (from_id) {
-        //let countFile = await CVideo.GetCount({from_id: from_id})
         let countFile = await collection.count({from_id: from_id})
-        /*await CUser.Edit(from_id, {count: {
-                video_out: countFile
-            }})*/
         let fields = {}
         fields[`count.${collectionName}_out`] = Number(countFile)
         await CUser.Edit(from_id, fields)
     }
     if (to_user_id) {
-        //let countFile = await CVideo.GetCount({from_id: to_user_id})
-        let countFile = await collection.count({from_id: to_user_id})
-        /*await CUser.Edit(to_user_id, {count: {
-                video_in: countFile
-            }})*/
+        let countFile = await collection.count({to_user_id: to_user_id})
         let fields = {}
         fields[`count.${collectionName}_in`] = Number(countFile)
         await CUser.Edit(to_user_id, fields)
     }
     if (to_group_id) {
-        //let countFile = await CVideo.GetCount({to_group_id: to_group_id})
         let countFile = await collection.count({to_group_id: to_group_id})
-        /*await CGroup.Edit(to_group_id, {count: {
-                video_in: countFile
-            }})*/
         let fields = {}
         fields[`count.${collectionName}_in`] = Number(countFile)
         await CGroup.Edit(to_group_id, fields)
